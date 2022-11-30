0bb7835d9c93440000268f8342480f3bcecb1a9d
danicorpa <daniel.martin385@educa.madrid.org>
2022-11-30 18:03:42 +0100

    Initial commit

---
+++ src/String/Ejercicio07.java
@@ -0,0 +1,21 @@
+package String;
+
+public class Ejercicio07 {
+    public static void main(String[] args) {
+
+        /***
+         * @author: Daniel Martin Martin
+         *
+         * Ejercicio 7
+         * Escribe un programa que lea una cadena y dos números enteros para indicar los límites
+         * inferior y superior respectivamente de un rango dentro de las posiciones de los caracteres de
+         * la cadena. Imprime la subcadena indicada por el rango:
+         * a) suponiendo que ambos números siempres son mayores o iguales a 0 y menores que la
+         * longitud de la cadena;
+         * b) Validando que sucede a).
+         *
+         * **/
+
+
+    }
+}
